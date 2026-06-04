493aa5c593339c39fee99a5143a1f55d8bc037ad
Anderson Sutil <adsslipknot@gmail.com>
2026-06-04 16:03:07 -0300

    mend

--- src/components/BackgroundBanner/index.tsx
+++ src/components/BackgroundBanner/index.tsx
@@ -1,5 +1,6 @@
 import HeroBanner, { HeroBannerProps } from "../HeroBanner";
 
-const BackgroundBanner = ({ backgroundImage, ...props }: HeroBannerProps) => <HeroBanner backgroundImage={backgroundImage} {...props} />;
+const BackgroundBanner = ({ backgroundImage, ...props }: HeroBannerProps) =>
+     <HeroBanner backgroundImage={backgroundImage} {...props} />;
 
 export default BackgroundBanner;
